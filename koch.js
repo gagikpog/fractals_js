b474ec855c09999fdaa73ef6eeb0da1a011d7052
Gagik <gagikpog@gmail.com>
2020-01-01 21:43:59 +0500

    add tabs

--- koch.js
+++ koch.js
@@ -1,40 +1,17 @@
-function Pos(x, y) {
-    return {
-        x, y,
-        getLength() {
-            return Math.sqrt(sqr(this.x) + sqr(this.y));
-        }
-    };
-}
-
-function sqr(x) {
-    return x*x;
-}
-let angle = 320 * Math.PI / 180;
-
-let canvas = null;
-let ctx = null;
-document.addEventListener("DOMContentLoaded", function() {
-    canvas = document.getElementById("draw");
-    ctx = canvas.getContext("2d");
-
-    drawFractal(700);
-});
 
 function tg(start, end, n = 0) {
-    
-    const L = Math.sqrt(sqr(start.x - end.x) + sqr(start.y - end.y));
+
+    const L = start.getDistance(end);
     const l = L / (2*(Math.cos(angle) + 1));
     const basicA = Pos((end.x - start.x) / L, (end.y - start.y) / L);
-    
+
     const B = Pos(start.x + basicA.x * l, start.y + basicA.y * l);
-    
+
     const I = L-l-l;
     const x1 = basicA.x * Math.cos(angle) - basicA.y * Math.sin(angle);
     const y1 = basicA.x * Math.sin(angle) + basicA.y * Math.cos(angle);
     const C = Pos(B.x + x1 * l,B.y + y1 * l);
-    
-    
+
     const D = Pos(end.x - basicA.x * l, end.y - basicA.y * l);
 
     if (!n) {
@@ -54,24 +31,23 @@ function tg(start, end, n = 0) {
     }
 }
 
-function drawFractal(length) {
+function drawKoch(config) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     const centerX = canvas.width / 2;
     const centerY = canvas.height / 2;
 
-    const _angle = +(document.querySelector('#angle').value || 60);
-    angle = _angle * Math.PI / 180;
-    const h = Math.sqrt(3 * sqr(length)/4);
-    const A = Pos(centerX - length / 2, centerY + h / 3);
-    const B = Pos(centerX + length / 2, centerY + h / 3);
+    setAngle(config.angle);
+    const h = Math.sqrt(3 * sqr(config.length)/4);
+    const A = Pos(centerX - config.length / 2, centerY + h / 3);
+    const B = Pos(centerX + config.length / 2, centerY + h / 3);
     const C = Pos(centerX, centerY - h * 2 / 3);
-    const n = +(document.querySelector('#deep').value || 5);
-    tg(A, B, n);
-    tg(C, A, n);
-    tg(B, C, n);
+
+    tg(A, B, config.deep);
+    tg(C, A, config.deep);
+    tg(B, C, config.deep);
 }
 
-function show() {
-    const length = +(document.querySelector('#size').value || 700);
-    drawFractal(length);
+function printKoch() {
+    config = getConfig();
+    drawKoch(config);
 }
---
+++ main.js
@@ -0,0 +1,63 @@
+let angle = 320 * Math.PI / 180;
+let canvas = null;
+let ctx = null;
+let currentTab = 'koch';
+
+function Pos(x, y) {
+    return {
+        x, y,
+        getLength() {
+            return Math.sqrt(sqr(this.x) + sqr(this.y));
+        },
+        getDistance(pos) {
+            return Math.sqrt(sqr(this.x - pos.x) + sqr(this.y - pos.y));
+        },
+        getBasic() {
+            const L = this.getLength();
+            return Pos(this.x / L, this.y / L);
+        }
+    };
+}
+
+function sqr(x) {
+    return x * x;
+}
+
+function setAngle(_angle) {
+    return angle = _angle * Math.PI / 180;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    canvas = document.getElementById("draw");
+    ctx = canvas.getContext("2d");
+    const config = getConfig()
+    drawKoch(config);
+});
+
+function getConfig() {
+    const deep = +(document.querySelector('#deep').value || 5);
+    const angle = +(document.querySelector('#angle').value || 60);
+    const length = +(document.querySelector('#size').value || 700);
+    return {deep, angle, length};
+}
+
+function onPaint() {
+    switch (currentTab) {
+        case 'koch':
+                printKoch();
+            break;
+        default:
+            break;
+    }
+}
+
+function changeTab(e) {
+    tabs = document.querySelectorAll('.tab') || [];
+    tabs.forEach(btn => {
+        btn.classList.remove('active');
+    });
+
+    currentTab = e.id;
+    e.classList.add('active')
+    onPaint();
+}
